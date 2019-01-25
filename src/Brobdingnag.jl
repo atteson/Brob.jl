d89dfe65ef2b17a0cd40397f48a9363df89523ca
Kevin Atteson <kevin@atteson.com>
2019-01-25 11:34:30 -0500

    fix NaN to be positive

--- src/Brobdingnag.jl
+++ src/Brobdingnag.jl
@@ -7,11 +7,12 @@ struct Brob <: Real
     log::Float64
 end
 
-Brob( x::T ) where {T <: Real} = Brob( sign(x) > 0, log(abs(x)) )
+Brob( x::T ) where {T <: Real} = Brob( !(sign(x) < 0), log(abs(x)) )
 
 Base.convert( ::Type{Brob}, x::Float64) = Brob( x )
 
 Base.convert( ::Type{Float64}, x::Brob) = (x.positive ? 1 : -1 )*exp( x.log )
+Base.convert( ::Type{Float32}, x::Brob) = Float32(convert(Float64, x))
 
 function Base.:+( x::Brob, y::Brob )
     s = x.positive == y.positive ? 1 : -1
@@ -47,10 +48,10 @@ Base.ones( ::Type{Brob}, n::Int ) = fill( Brob(true, 0.0), n )
 Base.:<( x::Brob, y::Brob ) = ( x.positive < y.positive ) || xor( !x.positive, ( x.log < y.log ) )
 Base.:<=( x::Brob, y::Brob ) = ( x.positive < y.positive ) || xor( !x.positive, ( x.log <= y.log ) )
 
-Base.promote_rule( ::Type{Brob}, ::Union{Type{Float64},Type{Int}} ) = Brob
+Base.promote_rule( ::Type{Brob}, ::Union{Type{Float64},Type{Int},Type{Float32}} ) = Brob
 
 function Base.sqrt( x::Brob )
-    @assert( x.positive )
+    @assert( x.positive, "Trying to take sqrt of $x" )
     return Brob( true, 0.5*x.log )
 end
 
